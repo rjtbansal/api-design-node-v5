ecc5cc786a06e3a1093e3247d8be97000c54afb1
Rajat Bansal <rjtbansal@gmail.com>
2026-07-02 19:19:46 -0400

    installed tsx to recognize typescript files and added env vars

--- env.ts
+++ env.ts
@@ -12,14 +12,14 @@ if (isDevelopment) {
 } else if (isTesting) {
   loadEnv('test')
 }
-
+// zod helps us validate the environment variables and provide type safety at runtime. It also allows us to provide default values and custom error messages for invalid environment variables.
 const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
-
+// Enums mainly for APP_STAGE, NODE_ENV is for compatibility with other libraries
   APP_STAGE: z.enum(['dev', 'test', 'production']).default('dev'),
-
+  // environment variables expect string values but we need to coerce them to numbers that are app will expect
   PORT: z.coerce.number().positive().default(3000),
   DATABASE_URL: z.string().startsWith('postgresql://'),
   JWT_SECRET: z.string().min(32, 'Must be 32 chars long'),
@@ -27,6 +27,7 @@ const envSchema = z.object({
   BCRYPT_ROUNDS: z.coerce.number().min(10).max(20).default(12),
 })
 
+// infer the type of the environment variables from the schema 
 export type Env = z.infer<typeof envSchema>
 let env: Env
 
@@ -41,7 +42,7 @@ try {
       const path = err.path.join('.')
       console.log(`${path}: ${err.message}`)
     })
-
+    // way to exit the process with a non-zero exit code to indicate failure
     process.exit(1)
   }
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
-
 const app = express();
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
 });
+
+// export app for use in other modules (like testing)
+export { app };
 
 export default app;
